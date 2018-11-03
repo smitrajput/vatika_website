cb273025aa3b6508f9666b9e5c053a7a040f0a9c
Raghu Paredla <raghuram5910@gmail.com>
2018-11-03 13:14:06 +0530

    Update settings.py

--- vatika_website/settings.py
+++ vatika_website/settings.py
@@ -133,7 +133,7 @@ LOGOUT_REDIRECT_URL = "home:home-page"
 
 EMAIL_HOST = 'smtp-mail.outlook.com.'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'Raghu5910@outlook.com'
-EMAIL_HOST_PASSWORD = 'imvirat@18'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
