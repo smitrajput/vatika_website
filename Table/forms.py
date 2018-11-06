b27229c72e4f1797991b641230fbdc972e640d0b
raghu5910 <raghuram5910@gmail.com>
2018-11-06 15:24:10 +0530

    datepicker

--- Table/forms.py
+++ Table/forms.py
@@ -4,6 +4,10 @@ from .models import BookTable
 
 
 class BookTableForm(forms.ModelForm):
+    
     class Meta:
         model = BookTable
         fields = ('customer', 'contact','email','total_persons','date','time')
+        widgets = {
+            'date': forms.DateInput(attrs={'class':'datepicker'}),
+        }
