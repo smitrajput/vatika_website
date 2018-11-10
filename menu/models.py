c9808ca9cb01ccbba790ba60d221b0a086851557
raghu5910 <raghuram5910@gmail.com>
2018-11-10 20:20:44 +0530

    Menu_choices added

--- menu/models.py
+++ menu/models.py
@@ -2,7 +2,16 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+
 class Menu(models.Model):
+    MENU_CHOICES = (
+        ('BreakFast','BreakFast'),
+        ('Meals', 'Meals'),
+        ('Dinner', 'Dinner'),
+        ('Special', 'Special'),
+        ('All', 'All'),
+    )
+    item_type = models.CharField(choices=MENU_CHOICES,default='All',max_length=20)
     item_name = models.CharField(max_length=100)
     item_price = models.CharField(max_length=5)
     item_description = models.TextField(default='tasty')
