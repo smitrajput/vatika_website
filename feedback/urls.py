7100942b6ff4b848d7ed9352a29f71061965abbc
raghu5910 <raghuram5910@gmail.com>
2018-11-11 20:25:42 +0530

    block_banner

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.CreateFeedback.as_view(),name = 'template'),
+    path('',views.CreateFeedback.as_view(),name = 'feedback'),
 ]
