46163001374ab45ac575257b5a59586e7e58347c
raghu5910 <raghuram5910@gmail.com>
2018-11-11 15:14:22 +0530

    nope

--- menu/models.py
+++ menu/models.py
@@ -15,7 +15,7 @@ class Menu(models.Model):
     item_type = models.CharField(choices=MENU_CHOICES,default='All',max_length=20)
     item_name = models.CharField(max_length=100)
     item_price = models.CharField(max_length=5)
-    item_description = models.TextField(blank=True)
+    item_description = models.TextField(default='tasty')
 
     def get_absolute_url(self):
         return reverse("menu:menu_list")
